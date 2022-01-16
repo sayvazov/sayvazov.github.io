3ddc8ef96706412368a7e4c52e37d82f69872573
Sasha Ayvazov <sayvazov@hey.com>
2022-01-16 14:55:47 -0500

    Still trying

--- js/form-actions.js
+++ js/form-actions.js
@@ -1,17 +1,29 @@
 server_url = "http://127.0.0.1:5000"
 
-async function searchName() {
+function searchName() {
     nameToSearch = document.getElementById("name-to-search-for");
-    console.log("Searching for " + nameToSearch.value);
-    console.log(nameToSearch)
+    
     var url = server_url + "/search/" + nameToSearch.value
     console.log(url)
-    var response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
-    if (response.ok){
-        console.log(response);
-    }
-    else {
-        console.log(response.statusText);
-    }
+    // var response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
+    // if (response.ok){
+    //     console.log(response);
+    // }
+    // else {
+    //     console.log(response.statusText);
+    // }
     //console.log(response);
+    fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+    .then(function(response) {
+        if (response.status !== 200) {
+            console.log("Problem: " + response.statusText);
+            return;
+        }
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    })
+    .catch(function(err) {
+        console.log("Fetch Error :-S", err);
+    })
 }
